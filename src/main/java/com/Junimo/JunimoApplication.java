f89e67967510afbf4abac9ba3d89f4b42c0098cc
fernancastillo <fern.castillo@duocuc.cl>
2025-11-07 11:55:49 -0300

    Actualización - Se crea la base del esqueleto del proyecto

---
+++ src/main/java/com/Junimo/JunimoApplication.java
@@ -0,0 +1,13 @@
+package com.Junimo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JunimoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JunimoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Junimo/JunimoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Junimo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JunimoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
